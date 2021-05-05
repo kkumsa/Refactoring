478531352df7a9c507dbf4aa8bad431ca667bb85
kkumsa <35092418+kkumsa@users.noreply.github.com>
2021-05-05 22:31:24 +0900

    make volumeCreditsFor()

--- refactoring.js
+++ refactoring.js
@@ -27,26 +27,33 @@ function statement(invoice, plays) {
   }
   
 
+  function volumeCreditsFor(perf) {
+    let result = 0;
+    result += Math.max(perf.audience - 30, 0);
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ("comedy" === playFor(perf).type) 
+    result += Math.floor(perf.audience / 5);
+    return result
+  }
 
-
+  
+  
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", 
-                                        minimumFractionDigits: 2 }).format;
+  const format = new Intl.NumberFormat("en-US", { 
+                                                  style: "currency", 
+                                                  currency: "USD", 
+                                                  minimumFractionDigits: 2 
+                                                }
+                                      ).format;
   for (let perf of invoice.performances) {
 
-    let thisAmount = amountFor(perf, playFor(perf));
-
-    volumeCredits += Math.max(perf.audience - 30, 0);
-
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === playFor(perf).type) 
-      volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
-    result += ` - ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience}석)\n`
-    totalAmount += thisAmount;
+    result += ` - ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`
+    totalAmount += amountFor(perf);
 
   }
 
